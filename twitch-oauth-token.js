3c21b0bfd3e2ca6c786fb9018aba487e3e21f89b
Aaron <mail@aosterwyk.com>
2022-12-31 11:52:19 -0600

    hide token text by default, changed onclick to addeventlistener

--- twitch-oauth-token.js
+++ twitch-oauth-token.js
@@ -1,19 +1,19 @@
 let fullUrl = window.location.hash.substring(1);
 let accessToken = (fullUrl.substring(fullUrl.search('#'), fullUrl.search('&'))).replace('access_token=', '');
+// let accessToken = "test token 32478923749328ecvisdbcv978w9h";
 
 console.log(accessToken);
-// document.getElementById('token').innerHTML = `Token:<br /><code id="tokenCodeBlock">${accessToken}</code>`;
-document.getElementById('token').innerHTML = `Token:<br /><code class="text-info" id="tokenCodeBlock">${accessToken}</code></span><br />`;
+document.getElementById('token').innerHTML = `Token<input class="form-control bg-dark text-light border-secondary mb-4" type="password" placeholder="" id="tokenCodeBlock" value="${accessToken}" readonly></input>`;
 
 // output example
 // https://acceptdefaults.com/twitch-oauth-token/
 // #access_token=TOKEN
 // &scope=channel%3Amoderate+chat%3Aedit+chat%3Aread+channel%3Aread%3Aredemptions&token_type=bearer
 
-function copyToken () {
+let copyButton = document.getElementById('copyButton');    
+copyButton.addEventListener('click', () => {
     let copyThis = document.getElementById('tokenCodeBlock');
-    let copyButton = document.getElementById('copyButton');    
-    navigator.clipboard.writeText(copyThis.innerText);
+    navigator.clipboard.writeText(copyThis.value);
     copyButton.classList.remove('btn-info');
     copyButton.classList.add('btn-success');
     copyButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
@@ -21,4 +21,24 @@ function copyToken () {
     <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
     <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
     </svg> Copied!`;
-}
+});
+
+let showHideButton = document.getElementById('showHideButton');
+showHideButton.addEventListener('click', () => {
+    let tokenCodeBlock = document.getElementById('tokenCodeBlock');
+    if(tokenCodeBlock.type == 'password') { 
+        tokenCodeBlock.type = "text";
+        showHideButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash" viewBox="0 0 16 16">
+        <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
+        <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
+        <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
+      </svg> Hide Token`;
+    }
+    else {
+        tokenCodeBlock.type = "password";
+        showHideButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+      </svg> Show Token`;
+    }
+});
